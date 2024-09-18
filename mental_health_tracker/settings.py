b219f11aee6a129c1f50fcb1f1769f794dd27638
farel39 <argyafarel.kasyara@gmail.com>
2024-09-18 13:44:29 +0700

    tutorial 3

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,3 +125,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://argya-farel-mentalhealthtracker.pbp.cs.ui.ac.id/", "https://argya-farel-mentalhealthtracker.pbp.cs.ui.ac.id/"]
+
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
